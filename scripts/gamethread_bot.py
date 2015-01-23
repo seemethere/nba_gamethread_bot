e48a00b98f52ef155098948ab3a9a657a5a62877
seemethere <seemethere101@gmail.com>
2015-01-23 16:42:30 -0600

    Finally added an interface! Setting groundwork for the future!

--- scripts/gamethread_bot.py
+++ scripts/gamethread_bot.py
@@ -7,9 +7,15 @@ import arrow
 import schedule
 import requests
 import json
+import curses
 
 current_date = None
 start_offset = 30 #in minutes
+games = []
+running = True
+help_txt = "(P)rint remaining games for today, (F)orce check games starting soon, (Q)uit"
+screen = curses.initscr(); screen.keypad(1)
+curses.noecho(); curses.cbreak()
 
 with open('../data/teams.json', 'r') as teams_file:
     teams_text = teams_file.read()
@@ -17,13 +23,14 @@ with open('../data/teams.json', 'r') as teams_file:
     teams = teams_json['teams']
 
 class Game:
-    def __init__(self, away, away_rec, home, home_rec, gametime):
+    def __init__(self, away, away_rec, home, home_rec, gametime, tv):
         self.away = away
         self.away_rec = away_rec
         self.home = home
         self.home_rec = home_rec
         self.gametime = gametime
         self.posted = False
+        self.tv = tv
 
     def get_time(self, tz='US/Eastern'):
         time = self.gametime.to(tz).datetime
@@ -46,40 +53,10 @@ class Game:
     def is_posted(self):
         return self.posted
 
-def get_current():
-    return arrow.now('US/Eastern')
+    def on_national_tv(self):
+        return self.tv != 'NBALP'
 
-def get_todays_games():
-    now = get_current().datetime
-    date_fmt = '{year}{month:02d}{day}'.format(year=now.year,
-                                               month=now.month,
-                                               day=now.day)
-    today_url = 'http://www.nba.com/gameline/{date}/'.format(date=date_fmt)
-    #today_url = 'http://www.nba.com/gameline/20150213/' #Error 403 Tester
-    r = requests.get(today_url)
-    if r.status_code == 200:
-        soup = BeautifulSoup(r.text)
-        times_base = soup.findAll('div', {'class':'nbaFnlStatTxSm'})
-        times = [str(element.string) for element in times_base]
-        if len(times) < 1: return None
-        matchups_base = soup.findAll('div', {'class': 'nbaPreMnButtDiv'})
-        matchups = [str(element)[54:61] for element in matchups_base]
-        i = 0
-        games = []
-        for matchup in matchups:
-            time = times[i].split(':')
-            away = find_team(matchup[0:3])
-            home = find_team(matchup[3:6])
-            gametime = arrow.now('US/Eastern').replace(hour=int(time[0][0:2]),
-                                                       minute=int(time[1][0:2]),
-                                                       second=0,
-                                                       microsecond=0)
-            gametime = convert_to_24(gametime, 'pm' in time[1])
-            games.append(Game(away, get_record(away), home, get_record(home), gametime))
-            i += 1
-        return sorted(games, key=lambda x: x.gametime) #Sorts by time, probably not necessary
-    else:
-        return r.status_code
+def get_current(): return arrow.now('US/Eastern')
 
 #Original arrow objects did not account for AM/PM, this is to remedy that
 def convert_to_24(time, is_PM):
@@ -117,17 +94,82 @@ def get_record(team):
                 return record
         print "ERROR: Couldn't find record for {} {}".format(loc, team['nickname'])
 
-if __name__ == '__main__':
-    games = get_todays_games()
-    if isinstance(games, int):
-        print 'ERROR[{}]'.format(games)
-        exit()
-    table = PrettyTable(['Time', 'Away', 'Home', 'Starting Soon'])
+def get_table_of_games(matches):
+    table = PrettyTable(['Time', 'Away', 'Home', 'Starting Soon', 'TV'])
     table.align['Home'] = table.align['Away'] = 'l'
-    for game in games:
+    for game in matches:
         hour, minute = game.get_time()
         table.add_row(['{:02d}:{:02d}'.format(hour, minute),
                        game.get_team(False),
                        game.get_team(True),
-                       game.starting_soon()])
-    print table
+                       game.starting_soon(),
+                       game.tv])
+    return table.get_string()
+
+def find_games_starting_soon():
+    for game in games:
+        if game.starting_soon():
+            None
+            #TODO: THIS IS WHERE THE POST TO REDDIT PART WILL GO
+
+def get_todays_games():
+    now = get_current().datetime
+    games[:] = []
+    date_fmt = '{year}{month:02d}{day}'.format(year=now.year,
+                                               month=now.month,
+                                               day=now.day)
+    today_url = 'http://www.nba.com/gameline/{date}/'.format(date=date_fmt)
+    #today_url = 'http://www.nba.com/gameline/20150213/' #Error 403 Tester
+    r = requests.get(today_url)
+    if r.status_code == 200:
+        soup = BeautifulSoup(r.text)
+        times_base = soup.findAll('div', {'class':'nbaFnlStatTxSm'})
+        times = [str(element.string) for element in times_base]
+        if len(times) < 1: return None
+        matchups_base = soup.findAll('div', {'class': 'nbaPreMnButtDiv'})
+        matchups = [str(element)[54:61] for element in matchups_base]
+        tv_stations = soup.findAll('div', {'class':'nbaLiveNetwkLogo'})
+        i = 0
+        for matchup in matchups:
+            time = times[i].split(':')
+            # Finds out the tv stations it's on
+            if 'nbaTNT' in  str(tv_stations[i]):
+                tv = 'TNT'
+            elif 'nbaESPN' in str(tv_stations[i]):
+                tv = 'ESPN'
+            else:
+                tv = 'NBALP'
+            away = find_team(matchup[0:3])
+            home = find_team(matchup[3:6])
+            gametime = arrow.now('US/Eastern').replace(hour=int(time[0][0:2]),
+                                                       minute=int(time[1][0:2]),
+                                                       second=0,
+                                                       microsecond=0)
+            gametime = convert_to_24(gametime, 'pm' in time[1])
+            games.append(Game(away, get_record(away), home, get_record(home), gametime, tv))
+            i += 1
+    else:
+        None
+        #TODO: Add error logging mechanism
+
+if __name__ == '__main__':
+    get_todays_games()
+    schedule.every().day.at('4:00').do(get_todays_games)
+    schedule.every(5).minutes.do(find_games_starting_soon)
+    while running:
+        screen.addstr(1, 0, "/r/NBA Gamethread Bot v0.2 by _seemethere")
+        screen.addstr(2, 0, help_txt, curses.COLOR_RED)
+        schedule.run_pending()
+        key = screen.getch()
+        if key == ord('q'):
+            screen.clear()
+            running = False
+        elif key == ord('p'):
+            screen.clear()
+            screen.addstr(3, 0, get_table_of_games(games))
+        elif key == ord('f'):
+            screen.clear()
+            #TODO: Add function to force compare games
+            screen.addstr('\nGames checked')
+        screen.refresh()
+    exit()
