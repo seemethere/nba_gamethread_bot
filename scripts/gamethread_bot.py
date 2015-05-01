485b35cbcd2eda3145750f9057990eb800851f50
seemethere <seemethere101@gmail.com>
2015-05-01 13:14:38 -0500

    Big update, got rid of ncurses

--- scripts/gamethread_bot.py
+++ scripts/gamethread_bot.py
@@ -9,18 +9,18 @@ import requests
 import json
 import curses
 
-current_date = None
+
 start_offset = 30 #in minutes
 games = []
 running = True
-help_txt = "(P)rint remaining games for today, (F)orce check games starting soon, (Q)uit"
-screen = curses.initscr(); screen.keypad(1)
-curses.noecho(); curses.cbreak()
 
 with open('../data/teams.json', 'r') as teams_file:
     teams_text = teams_file.read()
     teams_json = json.loads(teams_text)
     teams = teams_json['teams']
+
+with open('../data/templates/title.txt', 'r') as title_file:
+    title_text = title_file.read()
 
 class Game:
     def __init__(self, away, away_rec, home, home_rec, gametime, tv):
@@ -56,7 +56,8 @@ class Game:
     def on_national_tv(self):
         return self.tv != 'NBALP'
 
-def get_current(): return arrow.now('US/Eastern')
+def get_current():
+    return arrow.now('US/Eastern')
 
 #Original arrow objects did not account for AM/PM, this is to remedy that
 def convert_to_24(time, is_PM):
@@ -91,8 +92,8 @@ def get_record(team):
                 record = {}
                 record['wins'] = element.contents[3].string
                 record['losses'] = element.contents[5].string
-                return record
-        print "ERROR: Couldn't find record for {} {}".format(loc, team['nickname'])
+        return record
+    print "ERROR: Couldn't find record for {} {}".format(loc, team['nickname'])
 
 def get_table_of_games(matches):
     table = PrettyTable(['Time', 'Away', 'Home', 'Starting Soon', 'TV'])
@@ -107,17 +108,30 @@ def get_table_of_games(matches):
     return table.get_string()
 
 def find_games_starting_soon():
+    print "Searching for games starting soon..."
     for game in games:
         if game.starting_soon():
-            None
+            title = generate_title(game)
+            games.remove(game)
+            print "{}".format(title)
             #TODO: THIS IS WHERE THE POST TO REDDIT PART WILL GO
 
+def generate_title(game):
+    title = title_text
+    date_fmt = '{}/{}/{}'.format(game.gametime.month,
+                                 game.gametime.day,
+                                 game.gametime.year)
+    return title.format(away = game.get_team(False),
+                        home = game.get_team(True),
+                        date = date_fmt)
+
 def get_todays_games():
+    print "Getting today's games"
     now = get_current().datetime
     games[:] = []
-    date_fmt = '{year}{month:02d}{day}'.format(year=now.year,
-                                               month=now.month,
-                                               day=now.day)
+    date_fmt = '{year}{month:02d}{day:02d}'.format(year=now.year,
+                                                   month=now.month,
+                                                   day=now.day)
     today_url = 'http://www.nba.com/gameline/{date}/'.format(date=date_fmt)
     #today_url = 'http://www.nba.com/gameline/20150213/' #Error 403 Tester
     r = requests.get(today_url)
@@ -135,8 +149,12 @@ def get_todays_games():
             # Finds out the tv stations it's on
             if 'nbaTNT' in  str(tv_stations[i]):
                 tv = 'TNT'
+            elif 'nbaESPN2' in str(tv_stations[i]):
+                tv = 'ESPN2'
             elif 'nbaESPN' in str(tv_stations[i]):
                 tv = 'ESPN'
+            elif 'nbaNBATV' in str(tv_stations[i]):
+                tv = 'NBATV'
             else:
                 tv = 'NBALP'
             away = find_team(matchup[0:3])
@@ -153,23 +171,13 @@ def get_todays_games():
         #TODO: Add error logging mechanism
 
 if __name__ == '__main__':
+    version = 0.2
+    print "NBA Gamethread Bot v{} by seemethere has started...".format(version)
     get_todays_games()
     schedule.every().day.at('4:00').do(get_todays_games)
-    schedule.every(5).minutes.do(find_games_starting_soon)
+    schedule.every(1).minutes.do(find_games_starting_soon)
+    print "{}".format(get_table_of_games(games))
     while running:
-        screen.addstr(1, 0, "/r/NBA Gamethread Bot v0.2 by _seemethere")
-        screen.addstr(2, 0, help_txt, curses.COLOR_RED)
         schedule.run_pending()
-        key = screen.getch()
-        if key == ord('q'):
-            screen.clear()
-            running = False
-        elif key == ord('p'):
-            screen.clear()
-            screen.addstr(3, 0, get_table_of_games(games))
-        elif key == ord('f'):
-            screen.clear()
-            #TODO: Add function to force compare games
-            screen.addstr('\nGames checked')
-        screen.refresh()
+        sleep(30)
     exit()
